99a0e2bde3ee6c68c6de4e33c1be815fc867bc1b
Vanderlei Fernandes <vanderleifds.7000@hotmail.com>
2026-05-31 18:01:09 -0300

    feat: conexão backend com banco

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+import dotenv from 'dotenv';
+
+import db from './db.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'AdminFlow API Online',
+  });
+});
+
+app.get(
+  '/status',
+
+  async (req, res) => {
+    try {
+      await db.query('SELECT 1');
+
+      res.json({
+        status: 'online',
+
+        database: 'connected',
+      });
+    } catch {
+      res.status(500).json({
+        status: 'error',
+
+        database: 'disconnected',
+      });
+    }
+  }
+);
+
+app.listen(
+  process.env.PORT,
+
+  () => {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
+  }
+);
